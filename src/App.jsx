a4d595698dab29160c519b1cbe8f6de785957fd3
Luis JaramillO <ljaramillocanas@gmail.com>
2024-03-14 18:11:07 -0500

    Se agrega opcion de turnos y se organiza logica de tablero

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react"
 
 //Declaramos turnos
 const TURNS={
-  X: 'x',
-  O: 'o'
+  X: '✖️',
+  O: '⭕'
 }
 
 //creamos el tablero 
@@ -11,18 +11,40 @@ const TURNS={
 const board = Array(9).fill(null)
 
 //square o cuadrado del tablero
-const Square = ({children,updateBoard,index}) => {
+
+ const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+  
   return(
-    <div className="square">
+    
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
 
 function App(){
-  const [board,setBoard] = useState(Array(9).fill(null))
-
+  const [board,setBoard] = useState(
+    Array(9).fill(null)
+  )
   const [turn,setTurn] = useState (TURNS.X )
+  
+  const updateBoard = (index) => {
+   
+    //Marcar turno en board
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    // cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    
+
+  }
 
  return (
   <main className="board">
@@ -34,6 +56,7 @@ function App(){
             <Square 
               key={index}
               index={index}
+              updateBoard={updateBoard}
             >
                 {board[index]}
             </Square> 
@@ -43,6 +66,8 @@ function App(){
       }
     </section>
     <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
 
     </section>
     </main>
